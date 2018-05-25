d6ea26d5612f4bd86efa50da63677a9a85962374
wilquerlima <wtl@cin.ufpe.br>
2018-05-25 16:34:46 -0300

    criação das classes de login e cadastro

--- drCarango/app/src/main/java/doutor/carangoapp/gui/IntroduçãoActivity.java
+++ drCarango/app/src/main/java/doutor/carangoapp/gui/IntroduçãoActivity.java
@@ -1,15 +1,38 @@
 package doutor.carangoapp.gui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import doutor.carangoapp.R;
 
-public class IntroduçãoActivity extends AppCompatActivity {
+public class IntroduçãoActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn_entrar, btn_cadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introducao);
+
+        btn_entrar = findViewById(R.id.btn_entrar);
+        btn_cadastrar = findViewById(R.id.btn_cadastrar);
+
+        btn_entrar.setOnClickListener(this);
+        btn_cadastrar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_entrar:
+                startActivity(new Intent(this,LoginActivity.class));
+                break;
+            case R.id.btn_cadastrar:
+                startActivity(new Intent(this,CadastroActivity.class));
+                break;
+        }
     }
 }
---
+++ drCarango/app/src/main/java/doutor/carangoapp/gui/LoginActivity.java
@@ -0,0 +1,21 @@
+package doutor.carangoapp.gui;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import doutor.carangoapp.R;
+
+/**
+ * Created by wilqu on 25/05/2018.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_login);
+    }
+}
